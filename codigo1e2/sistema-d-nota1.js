b7dd872316c0afef229d66f28ebef2bd844ee442
widerlan <114496510+widerlan@users.noreply.github.com>
2022-12-17 20:17:47 -0300

    Add files via upload

---
+++ codigo1e2/sistema-d-nota1.js
@@ -0,0 +1,7 @@
+var nota1 = parseFloat ( prompt ( "Digite a primeira nota: " ) );
+var nota1 = parseFloat ( prompt ( "Digite a primeira nota: " ) );
+var nota2 = parseFloat ( prompt ("Digite a segunda nota: " ) );
+var media  =  ( nota1  +  nota2  +  nota3 ) / 3 ;
+
+var resultado = media  >=  7 ? alert ( "você foi aprovado" ) : alert ( "você foi reprovado" ) ;
+ 
---
+++ codigo1e2/sistema-d-nota2.js
@@ -0,0 +1,6 @@
+var nota1 = parseFloat ( prompt ( "Digite a primeira nota: " ) );
+var nota2 = parseFloat ( prompt ("Digite a segunda nota: " ) );
+var mídia  =  ( nota1  +  nota2 )
+medianecessaria  =  21  -  media ;
+
+var  resultado  =  media  >=  21 ? alert ( "você ja ta com media 7" ) : alert ( "Voce precisa tirar: " + medianecessaria ) 
